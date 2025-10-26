3ddc8e283578b4767209a4d87f79a72dc9de875e
Chris G <gomezc@whitman.edu>
2025-10-26 16:03:43 -0400

    removing main function

--- backend/src/text_embedding/embedding.py
+++ backend/src/text_embedding/embedding.py
@@ -25,15 +25,3 @@ class Embedding:
         norm_a=np.linalg.norm(vec1)
         norm_b=np.linalg.norm(vec2)
         return dot_product/(norm_a * norm_b)
-
-if __name__ == "__main__":
-    emb = Embedding()
-    text1 = "Graph Neural Networks in Healthcare"
-    text2 = "NLP in Medical Research"
-    text3 = "Paris is the capital of France"
-    emb_1 = emb.embed_text(text1)
-    emb_2 = emb.embed_text(text2)
-    emb_3 = emb.embed_text(text3)
-    print(f"Embedding: {emb.embed_text(text1)}")
-    print(f"Cosine similairty (should be similar): {emb.cosine_similarity(emb_1, emb_2)}")
-    print(f"Cosine similairty (should be different): {emb.cosine_similarity(emb_1, emb_3)}")
