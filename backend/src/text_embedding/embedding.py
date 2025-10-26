3c1cf35339a6e298e50d47f15167f67b064f9de5
Chris G <gomezc@whitman.edu>
2025-10-26 16:02:20 -0400

    semantic similarity draft

--- backend/src/text_embedding/embedding.py
+++ backend/src/text_embedding/embedding.py
@@ -5,21 +5,35 @@ from openai import OpenAI
 from dotenv import load_dotenv
 import json
 from langchain_openai import OpenAIEmbeddings
+import numpy as np
 
 load_dotenv()
 os.environ["OPENAI_API_KEY"]=os.getenv("OPENAI_API_KEY")
 
 class Embedding:
     def __init__(self):
-        self.embeddings = OpenAIEmbeddings()
+        self.embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
 
     def embed_text(self, text: str) -> List[float]:
         try:
             return self.embeddings.embed_query(text)
         except Exception as e:
             raise e
+    
+    def cosine_similarity(self,vec1, vec2):
+        dot_product=np.dot(vec1,vec2)
+        norm_a=np.linalg.norm(vec1)
+        norm_b=np.linalg.norm(vec2)
+        return dot_product/(norm_a * norm_b)
 
 if __name__ == "__main__":
     emb = Embedding()
-    text = "Graph Neural Networks in Healthcare"
-    print(f"Embedding: {emb.embed_text(text)}")
+    text1 = "Graph Neural Networks in Healthcare"
+    text2 = "NLP in Medical Research"
+    text3 = "Paris is the capital of France"
+    emb_1 = emb.embed_text(text1)
+    emb_2 = emb.embed_text(text2)
+    emb_3 = emb.embed_text(text3)
+    print(f"Embedding: {emb.embed_text(text1)}")
+    print(f"Cosine similairty (should be similar): {emb.cosine_similarity(emb_1, emb_2)}")
+    print(f"Cosine similairty (should be different): {emb.cosine_similarity(emb_1, emb_3)}")
